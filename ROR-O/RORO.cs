c7ea58bab19718a89160f5ea5eb26ab84f47ddb3
Bogdan <142807122+DeusSixik@users.noreply.github.com>
2026-05-10 01:25:14 +0300

    Setup project

---
+++ ROR-O/RORO.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ROR_O
+{
+    [BepInPlugin(Modguid, ModName, ModVersion)]
+    public sealed class RORO : BaseUnityPlugin
+    {
+        public static ManualLogSource? GlobalLogger { get; private set; }
+        
+        private const string Modguid = "net.sixik.plugin.roro";
+        private const string ModName = "ROR-O";
+        private const string ModVersion = "1.0.0";
+
+        private void Awake()
+        {
+            GlobalLogger = Logger;
+            GlobalLogger.LogInfo($"Initialize ROR-O mod: {ModName} v{ModVersion}");
+            
+            var harmony = new Harmony(Modguid);
+            harmony.PatchAll();
+            GlobalLogger.LogInfo("Harmony patches applied successfully!");
+        }
+    }
+}
